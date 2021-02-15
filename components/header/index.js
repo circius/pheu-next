8128d437bd38f42f2993c9fae7218fc018445c76
circius <circius@posteo.de>
2021-02-15 17:46:09 +0100

    header

--- components/header/index.js
+++ components/header/index.js
@@ -1,5 +1,14 @@
+import { headerLinks } from '../../config.json'
+import PageLinks from './pageLinks.js'
+import HomeLink from './homeLink.js'
+
 const Header = () => {
-  return <div className='Header'>Header</div>
+  return (
+    <div className='Header'>
+      <HomeLink />
+      <PageLinks links={headerLinks} />
+    </div>
+  )
 }
 
 export default Header
