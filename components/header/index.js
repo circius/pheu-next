958b9946e419ed1ae55a506fcfb733cd63127512
circius <circius@posteo.de>
2021-02-18 11:39:05 +0100

    add icons for github and contact

--- components/header/index.js
+++ components/header/index.js
@@ -12,7 +12,6 @@ const Header = () => {
           display: flex;
           justify-content: space-between;
           max-width: 1152px;
-          margin: auto;
                 }
       `}
       </style>
--- components/header/pageLinks.js
+++ components/header/pageLinks.js
@@ -1,11 +1,23 @@
 import Link from 'next/link'
+import { FaGithub, FaEnvelope } from 'react-icons/fa'
+
+const getLabel = link => {
+  switch (link.label) {
+    case "Github":
+      return <FaGithub />
+    case "Contact":
+      return <FaEnvelope />
+    default:
+      return link.label
+  }
+}
 
 const PageLinks = ({ links }) => {
   return (
     <div className='PageLinks'>
       {links.map(
         (link, idx) => <Link href={link.href} key={idx}>
-          <a>{link.label}</a>
+          <a>{getLabel(link)}</a>
         </Link>)
       }
       <style jsx>{`
